171fb88c05f03096c685a21426137c5f3c1ca447
Hideki Arima <hide210g@gmail.com>
2022-08-10 10:35:49 +0900

    feat: before-refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-first-refactoring';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<{ data: User[] }>('https://reqres.in/api/users')
+      .subscribe((resp) => {
+        this.users = resp.data;
+      });
+  }
 }
